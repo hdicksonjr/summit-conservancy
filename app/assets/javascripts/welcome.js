0c4571c9a9a08e723431de8f76efd9e4a60daf92
hdicksonjr <hcdicksonjr@gmail.com>
2013-11-20 00:40:42 -0500

    style

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
   console.log("above opn");
-  $("#subMenu").onePageNav();
-  console.log("below opn");
+  $("#nav").onePageNav({
+	    currentClass: 'current',
+	    changeHash: false,
+	    scrollSpeed: 750,
+	    scrollOffset: 30,
+	    scrollThreshold: 0.5,
+	    filter: '',
+	    easing: 'swing',
+	    begin: function() {
+  },
+		end: function() {},
+		scrollChange: function($currentListItem) { }	
+  });
 });
